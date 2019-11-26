2c4630175a699317390a5a91a658d31e9f8e4f74
wangshuo <ws15531085321@163.com>
2019-11-26 16:04:45 +0800

    权限

--- router/router_permissions.js
+++ router/router_permissions.js
@@ -38,7 +38,7 @@ router_permissions
       }
     })
   })
-  // 获取第一级导航
+  // 获取第一级权限分类
   .get('/api/permission/getFirPermissionList', (req, res) => {
     let sql = `select permissionsId, permissionsName from permissions where permissionsPId = 0`;
     conn.query(sql, (err, result) => {
@@ -49,7 +49,7 @@ router_permissions
       return res.send({code: 200, message: '数据获取成功', result: result});
     })
   })
-  // 添加导航
+  // 添加权限分类
   .post('/api/permission/addPermissionCate', (req, res) => {
     let sql = `insert into permissions set ?`;
     let data = {
@@ -67,7 +67,7 @@ router_permissions
       return res.send({code: 200, msg: '操作成功'})
     })
   })
-  // 删除导航
+  // 删除权限分类
   .get('/api/permission/deletePermissionCate', (req, res) => {
     let sql = 'delete from permissions where permissionsId = ?;delete from permissions where permissionsPId = ?';
     conn.query(sql, [parseInt(req.query.id), parseInt(req.query.id)], (err, result) => {
@@ -78,7 +78,7 @@ router_permissions
       return res.send({code: 200, msg: '操作成功'})
     })
   })
-  // 获取数据
+  // 获取权限分类数据
   .get('/api/permission/getPermissionData', (req, res) => {
     let sql = `select * from permissions where permissionsId = ?`;
     conn.query(sql, req.query.id, (err, result) => {
@@ -90,7 +90,7 @@ router_permissions
       return res.send({code: 200, message: '数据获取成功', result: result[0]});
     })
   })
-  // 编辑导航
+  // 编辑权限分类
   .post('/api/permission/editPermissionCate', (req, res) => {
     let sql = `update permissions permissions set ? where permissionsId = ?`;
     let data = {

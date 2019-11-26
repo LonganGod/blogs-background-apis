bb0836a32801a3bce6fab7992dbd575573cadeae
wangshuo <ws15531085321@163.com>
2019-11-26 15:59:47 +0800

    权限

--- router/router_permissions.js
+++ router/router_permissions.js
@@ -118,5 +118,138 @@ router_permissions
       return res.send({code: 200, message: '数据获取成功', result: result});
     })
   })
+  // 编辑权限
+  .post('/api/permission/editPermission', (req, res) => {
+    let pId = parseInt(req.body.permissionsId)
+    let newList = req.body.permissionList
+    let oldList = []
+    let updateList = []
+    let insertList = []
+    let deleteList = []
+
+    let sql1 = `select * from permissions where permissionsPId = ?`
+    let sql2 = `update permissions set ? where permissionsId = ?`
+    let sql3 = `insert into permissions set ?`
+    let sql4 = `delete from permissions where permissionsId = ?`
+
+    let errNum = 0
+    let uIndex, iIndex, dIndex
+    let canSend = true
+
+    conn.query(sql1, [pId], (err1, result1) => {
+      if (err1) {
+        console.log(err1)
+        errNum++
+      }
+      oldList = result1
+
+      for (let i = 0; i < oldList.length; i++) {
+        for (let j = 0; j < newList.length; j++) {
+          if (oldList[i].permissionsId == newList[j].permissionsId) {
+            updateList.push(newList[j])
+          }
+        }
+      }
+      for (let i = 0; i < oldList.length; i++) {
+        let num = 0
+        for (let j = 0; j < updateList.length; j++) {
+          if (oldList[i].permissionsId == updateList[j].permissionsId) {
+            num++
+          }
+        }
+        if (num == 0) {
+          deleteList.push(oldList[i])
+        }
+      }
+      for (let i = 0; i < newList.length; i++) {
+        let num = 0
+        for (let j = 0; j < updateList.length; j++) {
+          if (newList[i].permissionsId == updateList[j].permissionsId) {
+            num++
+          }
+        }
+        if (num == 0) {
+          insertList.push(newList[i])
+        }
+      }
+
+      for (uIndex = 0; uIndex < updateList.length; uIndex++) {
+        let data = {
+          permissionsName: updateList[uIndex].permissionsName,
+          permissionsUrl: updateList[uIndex].permissionsUrl,
+          permissionsPId: updateList[uIndex].permissionsPId,
+          type: '0',
+        }
+        conn.query(sql2, [data, updateList[uIndex].permissionsId], (err2, result2) => {
+          if (err2) {
+            errNum++
+            console.log(err2)
+          }
+          if (
+            uIndex == updateList.length &&
+            iIndex == insertList.length &&
+            dIndex == deleteList.length &&
+            canSend
+          ) {
+            canSend = false
+            if (errNum == 0) {
+              return res.send({code: 200, message: '操作成功'})
+            } else {
+              return res.send({code: 201, message: '操作失败'})
+            }
+          }
+        })
+      }
+      for (iIndex = 0; iIndex < insertList.length; iIndex++) {
+        let data = {
+          permissionsName: insertList[iIndex].permissionsName,
+          permissionsUrl: insertList[iIndex].permissionsUrl,
+          permissionsPId: pId,
+          type: '0',
+          createTime: new Date()
+        }
+        conn.query(sql3, [data], (err3, result3) => {
+          if (err3) {
+            errNum++
+            console.log(err3)
+          }
+          if (
+            uIndex == updateList.length &&
+            iIndex == insertList.length &&
+            dIndex == deleteList.length &&
+            canSend
+          ) {
+            canSend = false
+            if (errNum == 0) {
+              return res.send({code: 200, message: '操作成功'})
+            } else {
+              return res.send({code: 201, message: '操作失败'})
+            }
+          }
+        })
+      }
+      for (dIndex = 0; dIndex < deleteList.length; dIndex++) {
+        conn.query(sql4, [deleteList[dIndex].permissionsId], (err4, result4) => {
+          if (err4) {
+            errNum++
+            console.log(err4)
+          }
+          if (
+            uIndex == updateList.length &&
+            iIndex == insertList.length &&
+            dIndex == deleteList.length &&
+            canSend
+          ) {
+            canSend = false
+            if (errNum == 0) {
+              return res.send({code: 200, message: '操作成功'})
+            } else {
+              return res.send({code: 201, message: '操作失败'})
+            }
+          }
+        })
+      }
+    })
+  })
 
 module.exports = router_permissions;
